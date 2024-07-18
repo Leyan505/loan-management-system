91fdb2e5c57bcef9078e8780c092a8c3cd4ec4de
Leyan505 <andresguido2005@gmail.com>
2024-07-18 16:09:37 -0600

    Update database

---
+++ Migrations/20240718000948_UpdateUsers.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PrestamosCreciendo.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateUsers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "City",
+                table: "Users",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "City",
+                table: "Users",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
